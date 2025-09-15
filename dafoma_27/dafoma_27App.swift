cc49f722dea20bc802144fca88c7051568b32889
spustoff <vyacheslavpustov05@gmail.com>
2025-09-15 19:34:42 +0300

    1.4(fb1)

--- dafoma_27/dafoma_27App.swift
+++ dafoma_27/dafoma_27App.swift
@@ -8,15 +8,10 @@
 
 import SwiftUI
 import UIKit
-import AppsFlyerLib
-import AppTrackingTransparency
-import AdSupport
 
 @main
 struct dafoma_27App: App {
-    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+        
     var body: some Scene {
         WindowGroup {
             ContentView()
